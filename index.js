90ce8e4c3ef6ccf0e2c6e3d4ddc84295cc5174f7
0X123 <54817860+0xRepox@users.noreply.github.com>
2024-06-04 11:18:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+var partner1 = prompt("What is your name?");
+var partner2 = prompt("What is yout partner's name?");
+var percentage = Math.random() * 100;
+var percentage= Math.floor(percentage + 1);
 
+alert("The likelyhood of love between " + partner1 + " and " + partner2 + " is " + percentage + "%.");
