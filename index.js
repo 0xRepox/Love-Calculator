04acc6c5639dba714c1ac3691097e930639a5ef2
0X123 <54817860+0xRepox@users.noreply.github.com>
2024-06-05 10:39:13 +0100

    Update index.js
    enhancing complication

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ var partner2 = prompt("What is yout partner's name?");
 var loveScore = Math.random() * 100;
 var loveScore= Math.floor(loveScore + 1);
 
-alert("The likelyhood of love between " + partner1 + " and " + partner2 + " is " + loveScore + "%.");
+if (loveScore >= 70) {
+    alert("The love between " + partner1 + " and " + partner2 + " is very strong");
+} else {
+    alert("There is a likelyhood of love between " + partner1 + " and " + partner2 );
+}
+
